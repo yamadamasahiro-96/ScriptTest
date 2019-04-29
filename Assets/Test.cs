820b6d8836756c8d3e7d6dafc6190d34765a4395
mahogany eggplant <ym081014@gmail.com>
2019-04-29 15:21:07 +0900

    ScriptTest
    発展課題まで

---
+++ Assets/Test.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {        
+	private int hp = 100;          // 体力
+	private int power = 25; // 攻撃力
+	private int mp = 53;
+
+	// 攻撃用の関数
+	public void Attack() { 
+		Debug.Log( this.power + "のダメージを与えた" );
+	}
+
+	// 防御用の関数
+	public  void Defence(int damage) { 
+		Debug.Log( damage+"のダメージを受けた" );
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
+
+	public  void Magic(){
+		for (int lowmp = mp; lowmp > 0; lowmp -= 5) {
+			if (lowmp >= 5) {
+				Debug.Log ("魔法攻撃をした。残りMPは" + (lowmp - 5));
+			} else {
+				Debug.Log ("MPが足りないため魔法が使えない。");
+			}
+		}
+	}
+}
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+
+		// 攻撃用の関数を呼び出す
+		//lastboss.Attack();
+		// 防御用の関数を呼び出す
+		//lastboss.Defence(3);
+
+		lastboss.Magic ();
+
+		//Boss midboss = new Boss ();
+
+		//midboss.Attack();
+
+		//midboss.Defence(1);
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Test1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = new int[5];
+
+		array [0] = 5;
+		array [1] = 7;
+		array [2] = 11;
+		array [3] = 3;
+		array [4] = 2;
+
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (array[i]);
+		}
+		for (int i = 4; i >= 0; i--) {
+			Debug.Log (array[i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
